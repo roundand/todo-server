e86f2a07f3f9bd8a9980bfa2ec3685c9d856ee84
roundand <francis.norton@gmail.com>
2015-04-14 17:18:47 +0100

    added default connection string

--- app/config.js
+++ app/config.js
@@ -3,5 +3,6 @@ var config = {}
 config.mongo = {};
 
 // the following connection string should be replaced with something valid, eg from mongolabs.com
-config.mongo.connectionString = 'mongodb://{username}:{password}@{domain}:{port}/{db}';
+// see http://docs.mongodb.org/manual/reference/connection-string/ for reference
+config.mongo.connectionString = 'mongodb://todomongodb;
 module.exports = config;
