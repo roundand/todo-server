ff91ce526f30f4b1016cd5bec7627c58c01bfd31
roundand <francis.norton@gmail.com>
2015-05-25 08:43:05 +0100

    Used more idiomatic style for Express routing.

--- app/routes/todo.js
+++ app/routes/todo.js
@@ -5,58 +5,54 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var todoModel = require('../models/todo.js');
 
-/* GET /todo */
-router.get('/', function(req, res, next) {
-  console.log('GET/ ' + JSON.stringify(req.params))
-  todoModel.find(function(err, todos) {
-    if(err) {
-      return next(err);
-    }
-    res.json(todos);
+/* /todo/ GET and POST */
+router.route('/')
+  .get(function(req, res, next) {
+    console.log('GET/ ' + JSON.stringify(req.params))
+    todoModel.find(function(err, todos) {
+      if(err) {
+        return next(err);
+      }
+      res.json(todos);
+    });
+  })
+  .post(function(req, res, next) {
+    console.log('POST ' + JSON.stringify(req.params))
+    todoModel.create(req.body, function (err, post) {
+      if (err) {
+        return next(err);
+      }
+      res.json(post);
+    });
   });
-});
 
-/* POST /todo */
-router.post('/', function(req, res, next) {
-  console.log('POST ' + JSON.stringify(req.params))
-  todoModel.create(req.body, function (err, post) {
-    if (err) {
-      return next(err);
-    }
-    res.json(post);
+
+/* /todo/:id GET, PUT and DELETE */
+router.route('/:id')
+  .get(function(req, res, next) {
+    console.log('GET /:id ' + JSON.stringify(req.params))
+    todoModel.findById(req.params.id, function (err, post) {
+      if (err) {
+        return next(err);
+      }
+      res.json(post);
+    });
+  })
+  .put(function(req, res, next) {
+    console.log('PUT /:id ' + JSON.stringify(req.params))
+    console.log('PUT body ' + JSON.stringify(req.body))
+    todoModel.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });
+  })
+  .delete(function(req, res, next) {
+    console.log('DELETE /:id ' + JSON.stringify(req.params))
+    console.log('DELETE body ' + JSON.stringify(req.body))
+    todoModel.findByIdAndRemove(req.params.id, function (err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });
   });
-});
-
-
-/* GET /todo/id */
-router.get('/:id', function(req, res, next) {
-  console.log('GET /:id ' + JSON.stringify(req.params))
-  todoModel.findById(req.params.id, function (err, post) {
-    if (err) {
-      return next(err);
-    }
-    res.json(post);
-  });
-});
-
-/* PUT /todos/:id */
-router.put('/:id', function(req, res, next) {
-  console.log('PUT /:id ' + JSON.stringify(req.params))
-  console.log('PUT body ' + JSON.stringify(req.body))
-  todoModel.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
-
-/* DELETE /todos/:id */
-router.delete('/:id', function(req, res, next) {
-  console.log('DELETE /:id ' + JSON.stringify(req.params))
-  console.log('DELETE body ' + JSON.stringify(req.body))
-  todoModel.findByIdAndRemove(req.params.id, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
 
 module.exports = router;
